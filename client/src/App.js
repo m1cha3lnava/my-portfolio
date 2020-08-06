abbec4bb3f331d9625f89aabd46ca8be8a0d4d49
Michael Nava <M1cha3l.nava@gmail.com>
2020-08-06 02:48:05 -0400

    added react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect} from "react";
+import axios from "axios";
+
+function App() {
+  useEffect(() => {
+
+  }, [])
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
