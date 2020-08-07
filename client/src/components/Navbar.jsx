d9ef0269c48860b38e25368968b1afca469fac3d
Michael Nava <M1cha3l.nava@gmail.com>
2020-08-06 22:01:15 -0400

    added navbar and footer components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <header>
+                Navbar
+            </header>
+        );
+    }
+}
+
+export default Navbar;
--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <hi1>This is my Homepage</hi1>
+      <div className="container">
+      <Navbar />
+        <h1>This is my Homepage</h1>
+        <Footer />
       </div>
     );
   }
